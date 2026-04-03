c87a484ceb3a29c2e2f5676a97c719f7602536bf
amosleoamadi <amadiamos146@gmail.com>
2026-04-03 14:55:36 +0100

    changed the url

--- todo/urls.py
+++ todo/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     path('todo/', views.get_todos, name='get-todos'),
     path('todo/create/', views.create_todo, name='create-todo'),
-    path('todos/<str:pk>/', views.get_todo_detail, name='todo-detail'),
-    path('todos/<str:pk>/update/', views.update_todo, name='update-todo-full'),  # PUT
-    path('todos/<str:pk>/delete/', views.delete_todo, name='delete-todo'),
-    path('todos/<str:pk>/mark-done/', views.mark_todo_done, name='mark-done'),  # PATCH
-    path('todos/<str:pk>/toggle/', views.toggle_todo_status, name='toggle-todo'),
+    path('todo/<str:pk>/', views.get_todo_detail, name='todo-detail'),
+    path('todo/<str:pk>/update/', views.update_todo, name='update-todo-full'),  # PUT
+    path('todo/<str:pk>/delete/', views.delete_todo, name='delete-todo'),
+    path('todo/<str:pk>/mark-done/', views.mark_todo_done, name='mark-done'),  # PATCH
+    path('todo/<str:pk>/toggle/', views.toggle_todo_status, name='toggle-todo'),
 ]
